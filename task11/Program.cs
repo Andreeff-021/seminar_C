634138280ff969c5733c72464465740098023fcc
Владимир Андреев <andreeff.vladimir@yandex.ru>
2022-09-02 21:09:41 +0300

    task11

---
+++ task11/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и 
+// удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Random rand = new Random();
+int number = rand.Next(100, 1000);
+
+Console.WriteLine($"Сгенерировалось число {number}");
+
+int digit2 = number % 10 ;
+int digit1 = number / 100;
+int digit3 = digit1 * 10 + digit2;
+
+Console.WriteLine(digit3);
