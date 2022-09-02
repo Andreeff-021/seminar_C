c6de75d63cba5b6ceac87d13f8eea72fd6d77751
Владимир Андреев <andreeff.vladimir@yandex.ru>
2022-09-02 22:05:04 +0300

    task14

---
+++ task012/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ведите первое число");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите второе число");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int x = n1 % n2;
+
+if (n1 % n2 == 0)
+{
+    Console.WriteLine("Кратны");
+}
+else
+{
+   Console.WriteLine($"Остаток {x}"); 
+}
