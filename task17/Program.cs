6bda2c6e94379bcc3ab2fe1792515f3a98255d98
Владимир Андреев <andreeff.vladimir@yandex.ru>
2022-09-07 09:40:25 +0300

    task17

---
+++ task17/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите координату X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("Первая координатная плоскость");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("Вторая координатная плоскость");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("Третья координатная плоскость");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("Четвертая координатная плоскость");
+}
+else
+{
+    Console.WriteLine("X или Y равен нулю");
+}
