649dcd10729a5837c9f0280c29bcda6f4c988212
Владимир Андреев <andreeff.vladimir@yandex.ru>
2022-09-07 10:24:28 +0300

    task20 поменял названия координат

--- task20/Program.cs
+++ task20/Program.cs
@@ -3,13 +3,13 @@
 // A (3,6); B (2,1) -> 5,09
 // A (7,-5); B (1,-1) -> 7,21
 
-Console.WriteLine("Введите координаты X1 и Y1 по очереди");
-int x1 = Convert.ToInt32(Console.ReadLine());
-int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты XA и YA по очереди");
+int xa = Convert.ToInt32(Console.ReadLine());
+int ya = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите координаты X2 и Y2 по очереди");
-int x2 = Convert.ToInt32(Console.ReadLine());
-int y2 = Convert.ToInt32(Console.ReadLine());
+int xb = Convert.ToInt32(Console.ReadLine());
+int yb = Convert.ToInt32(Console.ReadLine());
 
-double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+double length = Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2));
 Console.WriteLine(Math.Round(length, 2));
