20baf412070d0bfddedb802558f79ab12d2ba23a
Владимир Андреев <andreeff.vladimir@yandex.ru>
2022-09-10 15:36:27 +0300

    task28

---
+++ task28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и 
+// выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Method2(int a)
+{
+    int prod = 1;
+    for (int i = 2; i <= a; i++)
+    {
+        prod = prod * i;
+    }
+    return prod;
+}
+
+int number = ReadNumber("Введите число: ");
+int n = Method2(number);
+Console.WriteLine($"Происведение равно {n}");
